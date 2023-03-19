444b65a4943810c1c39cbae08d63dea7df722e2d
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2023-03-20 12:40:17 +1300

    Create Task 2 - Labels v1.py

---
+++ Task 2 - Labels v1.py
@@ -0,0 +1,13 @@
+""" Intro to GUI - task 2
+Labels- font formatting
+"""
+from tkinter import *
+root = Tk()
+
+root.title("Welcome")
+welcome = Label(root, bg="black", fg="white", text="Welcome",
+                font=("Times", 50, "bold"))
+welcome.pack()
+
+
+root.mainloop()
